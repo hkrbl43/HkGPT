a3c74649d9814b73c1b69d9facd96d9eff5b84c9
Hari Krishna <107879733+hkrbl43@users.noreply.github.com>
2023-08-25 07:35:07 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -23,8 +23,7 @@ compile = False # use PyTorch 2.0 to compile the model to be faster
 exec(open('configurator.py').read()) # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
+
 torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
 torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
 device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
